b8859fcf9fa5f9fe86ebda317d011910d0aacd5f
Abel <abel@gentux.homenetwork>
2024-05-02 20:10:48 +0100

    Add nvim configurations

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,62 @@
+-- [[ Setting options ]]
+-- See `:help vim.opt'
+-- For more options, see `:help option-list'
+
+-- Add the line numbers and relative line numbers
+-- for conveniently jumping
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Enable mouse, can be useful for resizing splits.
+vim.opt.mouse = 'a'
+
+-- Do not show the mode, since it is already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+vim.opt.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital
+-- letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- Set indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+
+-- vim: ts=4 sts=4 sw=4 et
