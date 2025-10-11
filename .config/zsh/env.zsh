18d7c0c7561e309789ce19ac922fbaf4532c8413
Abel <abel@gentux.hostnamework>
2025-10-11 15:22:06 +0800

    Fix: Screenshot bug
    - bind the keymap to screenshot succefully
    - change the performance of zen
    - change lang setting: from C.UTF-8 to en_US.UTF-8 and zh_CN.UTF-8

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -1,4 +1,10 @@
+# Path
 export PATH="/usr/lib64/qt6/bin:$HOME/.cargo/bin:$PATH"
 export OLLAMA_MODELS="$HOME/Public/Model/Ollama"
 export EDITOR="nvim"
 export VISUAL="nvim"
+
+# Let Charset support Chinese
+export LANG=en_US.UTF-8
+export LC_CTYPE=zh_CN.UTF-8
+export LC_TIME=C.UTF-8
